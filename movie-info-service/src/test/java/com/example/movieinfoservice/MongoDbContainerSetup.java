9271bb23cb89368db2fc5a0db2f91442f1df1648
ch <mkmsacc@outlook.com>
2023-04-06 18:27:53 +0700

    Create abstract mongo db container setup

---
+++ movie-info-service/src/test/java/com/example/movieinfoservice/MongoDbContainerSetup.java
@@ -0,0 +1,19 @@
+package com.example.movieinfoservice;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class MongoDbContainerSetup {
+    @Container
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+    @DynamicPropertySource
+    static void mongoProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getConnectionString);
+        registry.add("spring.data.mongodb.database", () -> "database_name");
+    }
+}
