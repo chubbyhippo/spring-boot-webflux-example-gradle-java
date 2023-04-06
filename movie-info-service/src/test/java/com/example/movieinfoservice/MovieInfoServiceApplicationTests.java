3d9ad972ced5246453055a24de54dcd00a9688e1
ch <mkmsacc@outlook.com>
2023-04-06 20:20:14 +0700

    Add acceptance test

--- movie-info-service/src/test/java/com/example/movieinfoservice/MovieInfoServiceApplicationTests.java
+++ movie-info-service/src/test/java/com/example/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.movieinfoservice;
 
+import com.example.movieinfoservice.dto.MovieInfoDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class MovieInfoServiceApplicationTests {
+import java.time.LocalDate;
+import java.util.List;
 
-	@Test
-	void contextLoads() {
-	}
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MovieInfoServiceApplicationTests extends MongoDbContainerSetup {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    // Coverage cheating
+    void shouldRunMain() {
+        try {
+            MovieInfoServiceApplication.main(new String[]{});
+        } catch (Exception e) {
+            assertTrue(true);
+        }
+        assertTrue(true);
+    }
+
+    @Test
+    void shouldAddMovieInfo() {
+        var toBesaveMovieInfoDto = new MovieInfoDto(null,
+                "Nobody",
+                2021,
+                List.of("Bob Odenkirk", "Connie Nielsen"),
+                LocalDate.of(2021, 4, 13));
+
+        client.post()
+                .uri("/v1/movieinfos")
+                .bodyValue(toBesaveMovieInfoDto)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(MovieInfoDto.class)
+                .value(movieInfoDto -> {
+                    var id = movieInfoDto.id();
+                    assertThat(id).isNotNull();
+                });
+
+
+    }
 
 }
