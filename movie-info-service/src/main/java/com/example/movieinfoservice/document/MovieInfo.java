c6cf7ebb123acd567eab525ab17b51cb1726dc87
ch <mkmsacc@outlook.com>
2023-04-06 12:05:41 +0700

    Change to data class

--- movie-info-service/src/main/java/com/example/movieinfoservice/document/MovieInfo.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/document/MovieInfo.java
@@ -1,18 +1,24 @@
 package com.example.movieinfoservice.document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 import java.util.List;
 
+
 @Document
-public record MovieInfo(
-        @Id
-        String id,
-        String name,
-        Integer year,
-        List<String> cast,
-        LocalDate releaseDate
-) {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieInfo {
+    @Id
+    private String id;
+    private String name;
+    private Integer year;
+    private List<String> cast;
+    private LocalDate releaseDate;
 }
