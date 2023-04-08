8d2471dc137512b7ea09b6857134de08d6179c61
ch <mkmsacc@outlook.com>
2023-04-08 23:40:07 +0700

    Implement get movieinfos controller

--- movie-info-service/src/main/java/com/example/movieinfoservice/controller/MovieInfoController.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/controller/MovieInfoController.java
@@ -5,6 +5,7 @@ import com.example.movieinfoservice.service.MovieInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -12,11 +13,16 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class MovieInfoController {
 
-    private final MovieInfoService movieInfoService;
+    private final MovieInfoService service;
+
+    @GetMapping("/movieinfos")
+    public Flux<MovieInfoDto> getMovieInfos() {
+        return service.getMovieInfo();
+    }
 
     @PostMapping("/movieinfos")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<MovieInfoDto> addMovieInfo(@RequestBody MovieInfoDto movieInfoDto) {
-        return movieInfoService.addMovieInfo(movieInfoDto).log();
+        return service.addMovieInfo(movieInfoDto).log();
     }
 }
--- movie-info-service/src/test/java/com/example/movieinfoservice/controller/MovieInfoControllerTest.java
+++ movie-info-service/src/test/java/com/example/movieinfoservice/controller/MovieInfoControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.movieinfoservice.controller;
 
+import com.example.movieinfoservice.document.MovieInfo;
 import com.example.movieinfoservice.dto.MovieInfoDto;
 import com.example.movieinfoservice.service.MovieInfoService;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -25,6 +27,39 @@ class MovieInfoControllerTest {
     @MockBean
     private MovieInfoService service;
 
+    @Test
+    void shouldGetMovieInfoDtos() {
+        var movieInfoDtos = List.of(
+                new MovieInfoDto("1",
+                        "Nobody",
+                        2021,
+                        List.of("Bob Odenkirk", "Connie Nielsen"),
+                        LocalDate.of(2021, 4, 13)),
+                new MovieInfoDto("2",
+                        "John Wick: Chapter 4",
+                        2023,
+                        List.of("Keanu Reeves", "Donnie Yen"),
+                        LocalDate.of(2023, 3, 22)),
+                new MovieInfoDto("3",
+                        "Jason Bourne",
+                        2016,
+                        List.of("Matt Damon", "Tommy lee"),
+                        LocalDate.of(2016, 7, 28))
+        );
+
+        when(service.getMovieInfo()).thenReturn(Flux.fromIterable(movieInfoDtos));
+
+        var responseBody = client.get()
+                .uri("/v1/movieinfos")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(MovieInfoDto.class)
+                .returnResult()
+                .getResponseBody();
+
+        assertThat(responseBody).isNotNull();
+    }
     @Test
     void shouldAddMovieInfo() {
 
