f8130165e260734760a01fecaca53c10d5676d42
ch <mkmsacc@outlook.com>
2023-04-09 10:42:07 +0700

    Add setup and teardown for intg test

--- movie-info-service/src/test/java/com/example/movieinfoservice/MovieInfoServiceApplicationTests.java
+++ movie-info-service/src/test/java/com/example/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -1,6 +1,10 @@
 package com.example.movieinfoservice;
 
+import com.example.movieinfoservice.document.MovieInfo;
 import com.example.movieinfoservice.dto.MovieInfoDto;
+import com.example.movieinfoservice.repository.MovieInfoRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +22,9 @@ class MovieInfoServiceApplicationTests extends MongoDbContainerSetup {
     @Autowired
     private WebTestClient client;
 
+    @Autowired
+    private MovieInfoRepository repository;
+
     @Test
         // Coverage cheating
     void shouldRunMain() {
@@ -29,14 +36,48 @@ class MovieInfoServiceApplicationTests extends MongoDbContainerSetup {
         assertTrue(true);
     }
 
+    @BeforeEach
+    void setUp() {
+        var movieInfos = List.of(
+                new MovieInfo(null,
+                        "Nobody",
+                        2021,
+                        List.of("Bob Odenkirk", "Connie Nielsen"),
+                        LocalDate.of(2021, 4, 13)),
+                new MovieInfo(null,
+                        "John Wick: Chapter 4",
+                        2023,
+                        List.of("Keanu Reeves", "Donnie Yen"),
+                        LocalDate.of(2023, 3, 22)),
+                new MovieInfo(null,
+                        "Jason Bourne",
+                        2016,
+                        List.of("Matt Damon", "Tommy lee"),
+                        LocalDate.of(2016, 7, 28))
+        );
+
+        repository.saveAll(movieInfos)
+                .log()
+                .subscribe();
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll()
+                .subscribe();
+    }
+
     @Test
     void shouldGetMovieInfos() {
-       client.get()
-               .uri("/v1/movieinfos")
-               .exchange()
-               .expectStatus()
-               .isOk()
-               .expectBodyList(MovieInfoDto.class);
+
+
+        client.get()
+                .uri("/v1/movieinfos")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(MovieInfoDto.class)
+                .hasSize(3);
 
     }
 
