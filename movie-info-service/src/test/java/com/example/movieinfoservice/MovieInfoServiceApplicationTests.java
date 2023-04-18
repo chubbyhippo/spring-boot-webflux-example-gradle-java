67c52348aaec752be31e3321015c698438a5b8b2
ch <mkmsacc@outlook.com>
2023-04-18 10:07:32 +0700

    Refactor abstract test container

--- movie-info-service/src/test/java/com/example/movieinfoservice/MovieInfoServiceApplicationTests.java
+++ movie-info-service/src/test/java/com/example/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class MovieInfoServiceApplicationTests extends MongoDbContainerSetup {
+class MovieInfoServiceApplicationTests extends AbstractTestcontainers {
 
     @Autowired
     private WebTestClient client;
