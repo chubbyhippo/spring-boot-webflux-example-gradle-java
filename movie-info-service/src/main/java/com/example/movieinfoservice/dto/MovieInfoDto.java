6cef1b0ffa70943931c08bdb2502346fd3261bef
ch <mkmsacc@outlook.com>
2023-04-18 16:30:18 +0700

    Add movie info dto validation

--- movie-info-service/src/main/java/com/example/movieinfoservice/dto/MovieInfoDto.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/dto/MovieInfoDto.java
@@ -1,11 +1,18 @@
 package com.example.movieinfoservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDate;
 import java.util.List;
 
 public record MovieInfoDto(
         String id,
+        @NotBlank
         String name,
+        @NotNull
+        @Positive
         Integer year,
         List<String> actors,
         LocalDate releaseDate
---
+++ movie-info-service/src/test/java/com/example/movieinfoservice/dto/MovieInfoDtoTest.java
@@ -0,0 +1,72 @@
+package com.example.movieinfoservice.dto;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MovieInfoDtoTest {
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
+        try (var factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
+    }
+
+    private static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of("1",
+                        "",
+                        2021,
+                        List.of("Bob Odenkirk", "Connie Nielsen"),
+                        LocalDate.of(2021, 4, 13),
+                        1,
+                        "must not be blank"),
+                Arguments.of("1",
+                        "Nobody",
+                        null,
+                        List.of("Bob Odenkirk", "Connie Nielsen"),
+                        LocalDate.of(2021, 4, 13),
+                        1,
+                        "must not be null"),
+                Arguments.of("1",
+                        "Nobody",
+                        -9999,
+                        List.of("Bob Odenkirk", "Connie Nielsen"),
+                        LocalDate.of(2021, 4, 13),
+                        1,
+                        "must be greater than 0")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void shouldValidateFields(String id,
+                              String name,
+                              Integer year,
+                              List<String> actors,
+                              LocalDate releaseDate,
+                              int expectedViolations,
+                              String errorMessage) {
+
+        var movieInfoDto = new MovieInfoDto(id, name, year, actors, releaseDate);
+        var actualViolations = validator.validate(movieInfoDto);
+
+        assertThat(actualViolations).hasSize(expectedViolations);
+        assertThat(actualViolations.iterator().next().getMessage()).isEqualTo(errorMessage);
+
+
+    }
+
+
+}
