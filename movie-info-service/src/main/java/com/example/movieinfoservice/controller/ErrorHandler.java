24a2df5d7cfe718388727d245f5971ce6f8503d1
ch <mkmsacc@outlook.com>
2023-04-19 12:18:22 +0700

    Add error handler

---
+++ movie-info-service/src/main/java/com/example/movieinfoservice/controller/ErrorHandler.java
@@ -0,0 +1,36 @@
+package com.example.movieinfoservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@Slf4j
+public class ErrorHandler {
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<String> handleRequestBodyError(WebExchangeBindException exception) {
+        log.error("Exception caught in handleRequestBodyError : {}", exception.getMessage());
+        var error = exception.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(objectError -> Objects.requireNonNullElse(objectError.getDefaultMessage(), ""))
+                .sorted()
+                .collect(Collectors.joining(","));
+
+        log.error("Error is : {}", error);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(error);
+
+
+    }
+
+}
---
+++ movie-info-service/src/test/java/com/example/movieinfoservice/controller/ErrorHandlerTest.java
@@ -0,0 +1,49 @@
+package com.example.movieinfoservice.controller;
+
+import com.example.movieinfoservice.dto.MovieInfoDto;
+import com.example.movieinfoservice.service.MovieInfoService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@WebFluxTest
+class ErrorHandlerTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @MockBean
+    private MovieInfoService service;
+    @Test
+    void shouldValidateMovieInfoDtoWhenAdding() {
+
+
+        var invalidMovieInfoDto = new MovieInfoDto(null,
+                "",
+                -9999,
+                List.of("Bob Odenkirk", "Connie Nielsen"),
+                LocalDate.of(2021, 4, 13));
+
+        when(service.addMovieInfo(invalidMovieInfoDto)).thenReturn(null);
+        client.post()
+                .uri("/v1/movieinfos")
+                .bodyValue(invalidMovieInfoDto)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(String.class)
+                .value(s -> assertThat(s).isEqualTo("must be greater than 0,must not be blank"));
+
+    }
+}
