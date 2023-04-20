b2ca62fc32f00ecbe7d5088c7f68edc23086506c
ch <mkmsacc@outlook.com>
2023-04-20 11:06:48 +0700

    Implement find movie info by year

--- movie-info-service/src/main/java/com/example/movieinfoservice/repository/MovieInfoRepository.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/repository/MovieInfoRepository.java
@@ -2,6 +2,8 @@ package com.example.movieinfoservice.repository;
 
 import com.example.movieinfoservice.document.MovieInfo;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface MovieInfoRepository extends ReactiveMongoRepository<MovieInfo, String> {
+    Flux<MovieInfo> findMovieInfoByYear(Integer year);
 }
---
+++ movie-info-service/src/test/java/com/example/movieinfoservice/repository/MovieInfoRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.example.movieinfoservice.repository;
+
+import com.example.movieinfoservice.AbstractTestcontainers;
+import com.example.movieinfoservice.document.MovieInfo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@DataMongoTest
+class MovieInfoRepositoryTest extends AbstractTestcontainers {
+
+    @Autowired
+    private MovieInfoRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        var movieInfos = List.of(
+                new MovieInfo("1",
+                        "Nobody",
+                        2021,
+                        List.of("Bob Odenkirk", "Connie Nielsen"),
+                        LocalDate.of(2021, 4, 13)),
+                new MovieInfo("2",
+                        "John Wick: Chapter 4",
+                        2023,
+                        List.of("Keanu Reeves", "Donnie Yen"),
+                        LocalDate.of(2023, 3, 22)),
+                new MovieInfo("3",
+                        "Jason Bourne",
+                        2016,
+                        List.of("Matt Damon", "Tommy lee"),
+                        LocalDate.of(2016, 7, 28))
+        );
+
+        repository.saveAll(movieInfos)
+                .log()
+                .blockLast();
+    }
+
+    @Test
+    void shouldGetMovieInfoByYear() {
+        var movieInfoByYear = repository.findMovieInfoByYear(2023)
+                .log();
+        StepVerifier.create(movieInfoByYear)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll()
+                .block();
+    }
+
+}
