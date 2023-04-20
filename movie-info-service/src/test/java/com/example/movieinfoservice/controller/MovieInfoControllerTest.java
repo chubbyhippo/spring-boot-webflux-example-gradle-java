19012c13ce0b7f63e5e0cc0861e04fab34ffbc77
ch <mkmsacc@outlook.com>
2023-04-20 10:00:49 +0700

    Refactor controller test

--- movie-info-service/src/test/java/com/example/movieinfoservice/controller/MovieInfoControllerTest.java
+++ movie-info-service/src/test/java/com/example/movieinfoservice/controller/MovieInfoControllerTest.java
@@ -27,27 +27,26 @@ class MovieInfoControllerTest {
     @MockBean
     private MovieInfoService service;
 
-    private final List<MovieInfoDto> movieInfoDtos = List.of(
-            new MovieInfoDto("1",
-                    "Nobody",
-                    2021,
-                    List.of("Bob Odenkirk", "Connie Nielsen"),
-                    LocalDate.of(2021, 4, 13)),
-            new MovieInfoDto("2",
-                    "John Wick: Chapter 4",
-                    2023,
-                    List.of("Keanu Reeves", "Donnie Yen"),
-                    LocalDate.of(2023, 3, 22)),
-            new MovieInfoDto("3",
-                    "Jason Bourne",
-                    2016,
-                    List.of("Matt Damon", "Tommy lee"),
-                    LocalDate.of(2016, 7, 28))
-    );
 
     @Test
     void shouldGetMovieInfoDtos() {
-
+        List<MovieInfoDto> movieInfoDtos = List.of(
+                new MovieInfoDto("1",
+                        "Nobody",
+                        2021,
+                        List.of("Bob Odenkirk", "Connie Nielsen"),
+                        LocalDate.of(2021, 4, 13)),
+                new MovieInfoDto("2",
+                        "John Wick: Chapter 4",
+                        2023,
+                        List.of("Keanu Reeves", "Donnie Yen"),
+                        LocalDate.of(2023, 3, 22)),
+                new MovieInfoDto("3",
+                        "Jason Bourne",
+                        2016,
+                        List.of("Matt Damon", "Tommy lee"),
+                        LocalDate.of(2016, 7, 28))
+        );
         when(service.getMovieInfo()).thenReturn(Flux.fromIterable(movieInfoDtos));
         var responseBody = client.get()
                 .uri("/v1/movieinfos")
