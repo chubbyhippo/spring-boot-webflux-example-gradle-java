5fe551e288e751dcb47473e5d9f86f3f4f076696
ch <mkmsacc@outlook.com>
2023-04-27 13:35:53 +0700

    Add serialize test

---
+++ movie-info-service/src/test/java/com/example/movieinfoservice/dto/MovieInfoDtoJsonTest.java
@@ -0,0 +1,42 @@
+package com.example.movieinfoservice.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class MovieInfoDtoJsonTest {
+
+    @Autowired
+    private JacksonTester<MovieInfoDto> json;
+
+    @Test
+    void shouldSerialize() throws Exception {
+        var movieInfoDto = new MovieInfoDto("1",
+                "Nobody",
+                2021,
+                List.of("Bob Odenkirk", "Connie Nielsen"),
+                LocalDate.of(2021, 4, 13));
+
+        assertThat(json.write(movieInfoDto)).extractingJsonPathStringValue("@.id")
+                .isEqualTo(movieInfoDto.id());
+        assertThat(json.write(movieInfoDto)).extractingJsonPathStringValue("@.name")
+                .isEqualTo(movieInfoDto.name());
+        assertThat(json.write(movieInfoDto)).extractingJsonPathNumberValue("@.year")
+                .isEqualTo(movieInfoDto.year());
+        assertThat(json.write(movieInfoDto)).extractingJsonPathStringValue("@.actors[0]")
+                .isEqualTo(movieInfoDto.actors().get(0));
+        assertThat(json.write(movieInfoDto)).extractingJsonPathStringValue("@.actors[1]")
+                .isEqualTo(movieInfoDto.actors().get(1));
+        assertThat(json.write(movieInfoDto)).extractingJsonPathStringValue("@.releaseDate")
+                .isEqualTo(movieInfoDto.releaseDate().toString());
+    }
+
+
+}
