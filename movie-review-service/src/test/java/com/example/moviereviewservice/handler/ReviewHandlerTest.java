9c7278cbb119ad8bcc14f8620b520a1e798ae9be
ch <mkmsacc@outlook.com>
2023-04-29 22:18:34 +0700

    Implement get reviews handler test

--- movie-review-service/src/test/java/com/example/moviereviewservice/handler/ReviewHandlerTest.java
+++ movie-review-service/src/test/java/com/example/moviereviewservice/handler/ReviewHandlerTest.java
@@ -12,8 +12,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.reactive.function.server.MockServerRequest;
 import org.springframework.web.reactive.function.server.EntityResponse;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -53,5 +57,30 @@ class ReviewHandlerTest {
                 .verifyComplete();
 
         verify(repository, times(1)).save(any());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    void shouldGetReviews() {
+        when(repository.findAll()).thenReturn(Flux.fromIterable(
+                List.of(new Review("1", "1", "good", 7.0),
+                        new Review("2", "2", "better", 9.0),
+                        new Review("3", "3", "best", 8.0))));
+
+        var serverResponseMono = handler.getReviews();
+
+        StepVerifier.create(serverResponseMono)
+                .consumeNextWith(serverResponse -> {
+                    var responseEntity = (EntityResponse<Flux<ReviewDto>>) serverResponse;
+                    var entity = responseEntity.entity();
+
+                    assertThat(serverResponse.statusCode()).isEqualTo(HttpStatus.OK);
+                    StepVerifier.create(entity)
+                            .expectNextCount(3)
+                            .verifyComplete();
+
+                })
+                .verifyComplete();
+
     }
 }
