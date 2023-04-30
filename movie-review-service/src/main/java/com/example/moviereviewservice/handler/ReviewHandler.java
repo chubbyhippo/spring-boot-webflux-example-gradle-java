b9b5c93e3986e7208d1f057a9a358049a7dcac67
ch <mkmsacc@outlook.com>
2023-04-30 22:01:29 +0700

    Optimize import

--- movie-review-service/src/main/java/com/example/moviereviewservice/handler/ReviewHandler.java
+++ movie-review-service/src/main/java/com/example/moviereviewservice/handler/ReviewHandler.java
@@ -6,7 +6,6 @@ import com.example.moviereviewservice.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
