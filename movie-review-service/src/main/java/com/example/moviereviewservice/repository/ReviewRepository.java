7cb025074d61f12776392ebb6209dcd51ab3615b
ch <mkmsacc@outlook.com>
2023-05-08 22:13:05 +0700

    Implement find reviews by movieInfoId repo

--- movie-review-service/src/main/java/com/example/moviereviewservice/repository/ReviewRepository.java
+++ movie-review-service/src/main/java/com/example/moviereviewservice/repository/ReviewRepository.java
@@ -2,7 +2,10 @@ package com.example.moviereviewservice.repository;
 
 import com.example.moviereviewservice.document.Review;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface ReviewRepository extends ReactiveMongoRepository<Review, String> {
+
+    Flux<Review> findReviewsByMovieInfoId(String movieInfoId);
 
 }
---
+++ movie-review-service/src/test/java/com/example/moviereviewservice/repository/ReviewRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.example.moviereviewservice.repository;
+
+import com.example.moviereviewservice.AbstractTestcontainers;
+import com.example.moviereviewservice.document.Review;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+@DataMongoTest
+class ReviewRepositoryTest extends AbstractTestcontainers {
+
+    @Autowired
+    private ReviewRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        var reviews = List.of(
+                new Review("1", "1", "good", 9.0),
+                new Review("2", "2", "better", 8.0),
+                new Review("4", "2", "bad", 3.0),
+                new Review("3", "3", "best", 7.0)
+        );
+
+        repository.saveAll(reviews)
+                .log()
+                .blockLast();
+
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll()
+                .block();
+    }
+
+    @Test
+    void shouldFindReviewsByMovieInfoId() {
+        var movieInfoId = "2";
+        Flux<Review> reviewsByMovieInfoId = repository.findReviewsByMovieInfoId(movieInfoId);
+
+        StepVerifier.create(reviewsByMovieInfoId)
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+
+}
