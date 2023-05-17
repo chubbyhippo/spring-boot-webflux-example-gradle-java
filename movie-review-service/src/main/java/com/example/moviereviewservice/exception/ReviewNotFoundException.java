1235122b138995e3afba7db812ca2f1ab1fa627b
ch <mkmsacc@outlook.com>
2023-05-17 21:39:12 +0700

    Add ReviewNotFoundException

---
+++ movie-review-service/src/main/java/com/example/moviereviewservice/exception/ReviewNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.moviereviewservice.exception;
+
+public class ReviewNotFoundException extends RuntimeException{
+    public ReviewNotFoundException(String message) {
+        super(message);
+    }
+
+    public ReviewNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
