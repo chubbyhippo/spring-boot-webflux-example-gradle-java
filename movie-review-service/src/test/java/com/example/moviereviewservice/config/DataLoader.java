250ad8abdabdaaabb9c5be600f8be705f0c391b5
ch <mkmsacc@outlook.com>
2023-06-10 23:49:28 +0700

    Add data loader for movie review service

---
+++ movie-review-service/src/test/java/com/example/moviereviewservice/config/DataLoader.java
@@ -0,0 +1,35 @@
+package com.example.moviereviewservice.config;
+
+import com.example.moviereviewservice.document.Review;
+import com.example.moviereviewservice.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataLoader {
+
+    private final ReviewRepository repository;
+
+    @Bean
+    public ApplicationRunner loader() {
+        return args -> {
+            var reviews = List.of(
+                    new Review("1", "1", "good", 9.0),
+                    new Review("2", "2", "better", 8.0),
+                    new Review("3", "3", "best", 7.0),
+                    new Review("4", "3", "da best", 8.0)
+            );
+
+            repository.saveAll(reviews)
+                    .log()
+                    .blockLast();
+        };
+    }
+
+
+}
