9db854a09970005f5af93a901b9e1ab941b556cd
ch <mkmsacc@outlook.com>
2023-06-11 13:20:51 +0700

    Remove MovieInfoServiceContainersConfiguration.java

--- movie-info-service/src/test/java/com/example/movieinfoservice/config/MovieInfoServiceContainersConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.example.movieinfoservice.config;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.context.ImportTestcontainers;
-
-@TestConfiguration(proxyBeanMethods = false)
-@ImportTestcontainers(AbstractTestcontainers.class)
-public class MovieInfoServiceContainersConfiguration {
-}
