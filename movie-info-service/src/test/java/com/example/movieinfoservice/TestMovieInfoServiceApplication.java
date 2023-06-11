9d2dd49dd714268b784f0f41fefdf9063a4b6d32
ch <mkmsacc@outlook.com>
2023-06-11 13:21:38 +0700

    Refactor TestMovieInfoServiceApplication

--- movie-info-service/src/test/java/com/example/movieinfoservice/TestMovieInfoServiceApplication.java
+++ movie-info-service/src/test/java/com/example/movieinfoservice/TestMovieInfoServiceApplication.java
@@ -1,12 +1,16 @@
 package com.example.movieinfoservice;
 
-import com.example.movieinfoservice.config.MovieInfoServiceContainersConfiguration;
+import com.example.movieinfoservice.config.AbstractTestcontainers;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
+@TestConfiguration(proxyBeanMethods = false)
+@ImportTestcontainers(AbstractTestcontainers.class)
 public class TestMovieInfoServiceApplication {
     public static void main(String[] args) {
         SpringApplication.from(MovieInfoServiceApplication::main)
-                .with(MovieInfoServiceContainersConfiguration.class)
+                .with(TestMovieInfoServiceApplication.class)
                 .run(args);
     }
 }
