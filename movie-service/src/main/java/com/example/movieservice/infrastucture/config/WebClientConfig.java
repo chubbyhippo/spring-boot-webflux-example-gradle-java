05b84ca4457b6eef60d4bba4c0a3663726729d69
ch <mkmsacc@outlook.com>
2023-06-15 23:16:55 +0700

    Add webclient config for http interface

--- movie-service/src/main/java/com/example/movieservice/infrastucture/config/WebClientConfig.java
+++ movie-service/src/main/java/com/example/movieservice/infrastucture/config/WebClientConfig.java
@@ -1,14 +1,20 @@
 package com.example.movieservice.infrastucture.config;
 
+import com.example.movieservice.infrastucture.service.MovieInfoDtoService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class WebClientConfig {
-
     @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.build();
+    MovieInfoDtoService movieInfoDtoService() {
+        var webClient = WebClient.builder()
+                .baseUrl("http://localhost:8080/")
+                .build();
+        var factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(webClient)).build();
+        return factory.createClient(MovieInfoDtoService.class);
     }
 }
