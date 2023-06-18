ae6725ccc2d785c20caca11939f0d84b4049aab0
ch <mkmsacc@outlook.com>
2023-06-19 06:58:39 +0700

    Commit any changes

---
+++ movie-service/src/test/java/com/example/movieservice/config/AbstractTestcontainers.java
@@ -0,0 +1,31 @@
+package com.example.movieservice.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mockserver.client.MockServerClient;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MockServerContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+public abstract class AbstractTestcontainers {
+
+    private static final MockServerContainer mockServerContainer = new MockServerContainer(DockerImageName
+            .parse("mockserver/mockserver:5.15.0"));
+
+    private static MockServerClient mockServerClient;
+
+    @DynamicPropertySource
+    static void mongoProperties(DynamicPropertyRegistry registry) {
+
+        mockServerClient = new MockServerClient(mockServerContainer.getHost(),
+                mockServerContainer.getServerPort());
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        mockServerClient.reset();
+    }
+}
