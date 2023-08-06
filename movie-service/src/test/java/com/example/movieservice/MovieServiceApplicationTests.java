9cc776eb35e807e2527a905aeb08201264e8026a
ch <mkmsacc@outlook.com>
2023-08-06 17:15:28 +0700

    Add integration test

--- movie-service/src/test/java/com/example/movieservice/MovieServiceApplicationTests.java
+++ movie-service/src/test/java/com/example/movieservice/MovieServiceApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.movieservice;
 
+import com.example.movieservice.config.AbstractTestcontainers;
 import org.junit.jupiter.api.Test;
+import org.mockserver.model.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class MovieServiceApplicationTests {
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
-	@Test
-	void contextLoads() {
-	}
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MovieServiceApplicationTests extends AbstractTestcontainers {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void shouldGetMovieInfoById() {
+        mockServerClient.when(
+                        request().withPath("/v1/movieinfos/{id}")
+                                .withPathParameter("id", "1"))
+                .respond(response("""
+                        {
+                          	"id": "1",
+                           	"name": "Nobody",
+                           	"year": 2021,
+                           	"actors": [
+                           		"Bob Odenkirk",
+                           		"Connie Nielsen"
+                           	],
+                           	"releaseDate": "2021-04-13"
+                        }
+                        """)
+                        .withContentType(MediaType.APPLICATION_JSON));
+
+
+        webTestClient.get()
+                .uri("/{id}", "1")
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+    }
 
 }
