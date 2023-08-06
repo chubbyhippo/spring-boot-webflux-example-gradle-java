024a0fafdfb51c0e6a3be7b94214da585e15a7b0
ch <mkmsacc@outlook.com>
2023-08-06 17:08:00 +0700

    Add container annotation

--- movie-service/src/test/java/com/example/movieservice/config/AbstractTestcontainers.java
+++ movie-service/src/test/java/com/example/movieservice/config/AbstractTestcontainers.java
@@ -5,22 +5,25 @@ import org.mockserver.client.MockServerClient;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MockServerContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 public abstract class AbstractTestcontainers {
 
+    @Container
     private static final MockServerContainer mockServerContainer = new MockServerContainer(DockerImageName
             .parse("mockserver/mockserver:5.15.0"));
 
     public static MockServerClient mockServerClient;
 
     @DynamicPropertySource
-    static void mongoProperties(DynamicPropertyRegistry registry) {
+    static void mockServerProperties(DynamicPropertyRegistry registry) {
 
         mockServerClient = new MockServerClient(mockServerContainer.getHost(),
                 mockServerContainer.getServerPort());
+        System.out.println(mockServerContainer.getEndpoint());
         registry.add("infrastructure.url.movie-info-service", mockServerContainer::getEndpoint);
 
     }
