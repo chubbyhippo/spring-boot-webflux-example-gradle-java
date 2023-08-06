2268f0f72b21a6fc412b88605c0ac880ba15aed9
ch <mkmsacc@outlook.com>
2023-08-06 19:39:53 +0700

    Refactor, using assertj satisfies

--- movie-info-service/src/test/java/com/example/movieinfoservice/interfaces/rest/MovieInfoResourceJsonTest.java
+++ movie-info-service/src/test/java/com/example/movieinfoservice/interfaces/rest/MovieInfoResourceJsonTest.java
@@ -28,17 +28,17 @@ class MovieInfoResourceJsonTest {
                 LocalDate.of(2021, 4, 13));
 
         assertThat(json.write(movieInfoDto)).extractingJsonPathStringValue("@.id")
-                .isEqualTo(movieInfoDto.id());
+                .satisfies(input -> assertThat(input).isEqualTo(movieInfoDto.id()));
         assertThat(json.write(movieInfoDto)).extractingJsonPathStringValue("@.name")
-                .isEqualTo(movieInfoDto.name());
+                .satisfies(input -> assertThat(input).isEqualTo(movieInfoDto.name()));
         assertThat(json.write(movieInfoDto)).extractingJsonPathNumberValue("@.year")
-                .isEqualTo(movieInfoDto.year());
+                .satisfies(input -> assertThat(input).isEqualTo(movieInfoDto.year()));
         assertThat(json.write(movieInfoDto)).extractingJsonPathStringValue("@.actors[0]")
-                .isEqualTo(movieInfoDto.actors().get(0));
+                .satisfies(input -> assertThat(input).isEqualTo(movieInfoDto.actors().get(0)));
         assertThat(json.write(movieInfoDto)).extractingJsonPathStringValue("@.actors[1]")
-                .isEqualTo(movieInfoDto.actors().get(1));
+                .satisfies(input -> assertThat(input).isEqualTo(movieInfoDto.actors().get(1)));
         assertThat(json.write(movieInfoDto)).extractingJsonPathStringValue("@.releaseDate")
-                .isEqualTo(movieInfoDto.releaseDate().toString());
+                .satisfies(input -> assertThat(input).isEqualTo(movieInfoDto.releaseDate().toString()));
     }
 
     @Test
@@ -54,12 +54,14 @@ class MovieInfoResourceJsonTest {
                 }
                 """;
 
+        MovieInfoResource expected = new MovieInfoResource("1",
+                "Nobody",
+                2021,
+                List.of("Bob Odenkirk", "Connie Nielsen"),
+                LocalDate.of(2021, 4, 13));
+
         assertThat(json.parse(content)).usingRecursiveComparison()
-                .isEqualTo(new MovieInfoResource("1",
-                        "Nobody",
-                        2021,
-                        List.of("Bob Odenkirk", "Connie Nielsen"),
-                        LocalDate.of(2021, 4, 13)));
+                .satisfies(input -> assertThat(input).isEqualTo(expected));
 
 
     }
