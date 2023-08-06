0988bff24e0fb8a26ed565844f048d0a7c8f1234
ch <mkmsacc@outlook.com>
2023-08-06 12:36:39 +0700

    Update dto

--- movie-service/src/main/java/com/example/movieservice/domain/MovieInfoResource.java
+++ movie-service/src/main/java/com/example/movieservice/domain/MovieInfoResource.java
@@ -1,11 +1,16 @@
 package com.example.movieservice.domain;
 
-import java.time.LocalDate;
+import lombok.Builder;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder
 public record MovieInfoResource(
         String id,
         String name,
         Integer year,
+        List<String> actors,
         LocalDate releaseDate
 ) {
 }
